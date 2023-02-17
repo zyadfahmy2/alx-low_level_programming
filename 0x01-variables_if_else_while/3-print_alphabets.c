017a0ed8ba11d9922e9b1d1ffa2ac0d457496b77
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-02-17 05:53:11 -0800

    this file will print the alphabet small and capital letter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+        char alphabet;
+
+        alphabet = 'a';
+        while (alphabet <= 'z')
+        {
+                putchar(alphabet++);
+        }
+        alphabet = 'A';
+        while (alphabet <= 'Z')
+        {
+                putchar(alphabet++);
+        }
+	putchar('$');
+        putchar('\n');
+        return (0);
+}
+
