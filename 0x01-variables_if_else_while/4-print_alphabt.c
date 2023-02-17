2fdd2bde02a26993fd6f3950e402eb0765b348c5
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-02-17 15:30:43 -0800

    file that prints all the alphabet except characters (q,e)

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		if (alphabet != 'q' && alphabet != 'e')
+		{
+			putchar(alphabet++);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
