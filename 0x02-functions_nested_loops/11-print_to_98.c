c07ac90bf85f5bfd5043edd03696e0f098c0b658
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-02-21 09:02:07 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*print_to_98 - print the argument till 98
+*@n: start
+*
+*Return: void
+*/
+void print_to_98(int n)
+{
+	for(; n < 99; n++)
+	{
+		if (n == 98)
+		{
+			printf("%d\n",n);
+		}
+		else
+		{
+			printf("%d ",n);
+		}
+	}
+}
