52b95d5f2d3b20b92ce6c9056ef06488a7420a22
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-02-21 08:18:15 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+*jack_bauer - print times of the day hours and minutes
+*
+*Return: void
+*/
+void jack_bauer(void)
+{
+	char hours;
+	char minutes;
+
+	hours = 0;
+	minutes = 0;
+	while (hours != 24)
+	{
+		_putchar(((hours / 10) + '0');
+		_putchar((hours % 10) + '0');
+		_putchar((minutes / 10) + '0');
+		_putchar((minutes % 10) + '0');
+		minutes++;
+		if (minutes == 60)
+		{
+			minutes =0;
+			hours++;
+		}
+	}
+}
