ceeafdbbaf280c50d0618f31d8e23072051d6f96
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-02-21 02:17:41 -0800

    0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,3 @@
-#include <unistd.h>
-/**
-*_putchar - prints character to the output stream
-*@c: character to be printed
-*
-*Return: 0 (success) -1 (fail)
-*/
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-
-}
 /**
 * print_alphabet - print the small letters of the alphabet
 *
@@ -20,7 +8,7 @@ void print_alphabet(void)
 	int i;
 	char alphabet;
 
-	alphabet = 'A';
+	alphabet = 'a';
 	for (i = 0; i < 26; i++)
 	{
 		_putchar(alphabet++);
