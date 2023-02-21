f8a52ab3bed84fa3e5e4056939e200de49cf972a
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-02-21 08:35:11 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,11 +11,10 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		x = 0;
 		for (j = 0; j < 10; j++)
 		{
 			_putchar('0' + (char)(i*j));
-			if(j != 9)
+			if (j != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
