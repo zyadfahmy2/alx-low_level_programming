270a4207a963c4a121b76da3809fdf4084194da1
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-02-21 03:31:12 -0800

    6

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_sign - print the sign of a number
+*@n: first param
+*
+*Return 1 -> positive, 0 ->0, -1 -> negative
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}
