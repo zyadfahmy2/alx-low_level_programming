183fbbc32fe4ebe1ceb715682fcbf36fa8f8d781
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-02-23 05:53:56 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,3 @@
-#include "main.h"
 /**
 *_isupper - checks for uppercase character
 *
@@ -8,9 +7,5 @@
 */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	return (0);
+	return (c >= 65 && c <= 90)
 }
