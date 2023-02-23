26a5bac25c5d48d720a348e87515bd17c68ec6e2
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-02-23 07:52:24 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,16 +6,21 @@
  */
 void print_number(int n)
 {
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
+	unsigned int tmp;
 
-    if (n / 10)
-    {
-        print_number(n / 10);
-    }
+	if (n < 0)
+	{
+		tmp = -n;
+		_putchar('-');
+	} else
+	{
+		tmp = n;
+	}
 
-    _putchar((n % 10) + '0');
+	if (tmp / 10)
+	{
+		print_number(tmp / 10);
+	}
+
+	_putchar((tmp % 10) + '0');
 }
