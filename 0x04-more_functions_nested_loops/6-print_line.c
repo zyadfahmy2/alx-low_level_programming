0e2a1088403e5ea575d9f2e23294c0559e96d92f
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-02-23 06:29:57 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_line - draws a straight line in the terminal
+* @n: number of times the character _ should be printed
+* the line ends with a \n
+* If n is 0 or less, the function should only print \n
+*/
+void print_line(int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
