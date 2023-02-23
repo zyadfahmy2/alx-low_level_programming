7649518e8821a6c3f47f4cec25152e06fe068905
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-02-23 06:46:03 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -4,9 +4,12 @@
  * main - check the code
  *
  * Return: Always 0.
- */
+*/
 int main(void)
 {
-    more_numbers();
+    print_diagonal(0);
+    print_diagonal(2);
+    print_diagonal(10);
+    print_diagonal(-4);
     return (0);
-}
+} 
