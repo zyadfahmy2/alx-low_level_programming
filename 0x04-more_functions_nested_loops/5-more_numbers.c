8ca81ae26855743f4c9481814a2c3c07f5677bc7
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-02-23 06:26:32 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,10 +15,9 @@ void more_numbers(void)
 			if (j >= 10)
 			{
 				_putchar('0' + j / 10);
-				_putchar ('0' + j % 10);
 			}
-			else
-				_putchar('0' +  j);
+			_putchar ('0' + j % 10);
+			
 		}
 		_putchar('\n');
 	}
