5e5361e44a05a4dac3f0d5314fc3af0fcfb7eba5
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-02-27 02:45:52 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*rev_string - function that reverses a string
+*@s: string to be reversed
+*/
+void rev_string(char *s)
+{
+/* ahmed - > demha   , omar -> ramo*/
+	int length, i;
+	char tmp;
+
+	length = 0;
+	i = 0;
+	while (s != '\0')
+		length ++;
+	for (i = 0; i < length / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[length - 1 - i];
+		s[length - 1 - i] = tmp;
+	}
+}
