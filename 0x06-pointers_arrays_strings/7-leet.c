7a04f2c8e23a1d63bb667fab4357374ca90b5f99
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-03-03 15:36:35 -0800

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* leet - encodes a string in 1337
+* @s: string to be encoded
+*
+* Return: the resulting string
+*/
+char *leet(char *s)
+{
+	int i, j;
+
+	char *characters = "aAeEoOtTlL";
+	char *encoding = "4433007711";
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == characters[j])
+			{
+				s[i] = encoding[j];
+			}
+		}
+	}
+
+	return (s);
+}
