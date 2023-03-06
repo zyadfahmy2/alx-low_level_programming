d5cba1224797c6fe7d8fe4de46bb59254973a27b
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-03-06 04:48:42 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,7 +7,7 @@
 * Return: the number of bytes in the initial segment of s 
 * which consist only of bytes from accept
 */
-int _strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, count, flag;
 
