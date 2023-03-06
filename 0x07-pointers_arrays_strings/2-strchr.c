3878cb4eb8e573ddd83a0c3d3cc9a82b6f79344a
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-03-06 03:23:56 -0800

    reverting changes

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,18 +10,10 @@
 */
 char *_strchr(char *s, char c)
 {
-		int a;
-
-		while (1)
-		{
-			a = *s++;
-			if (a == c)
-			{
-				return (s - 1);
-			}
-			if (a == 0)
-			{
-				return (NULL);
-			}
-		}
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+			return (s);
+	}
+	return (NULL);
 }
