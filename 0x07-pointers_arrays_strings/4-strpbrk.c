641883959a76601696671c77657ea8bfd4ebff0d
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-03-06 04:58:48 -0800

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* _strbrk - function that searches a string for any of a set of bytes.
+* *_strbrk - function that searches a string for any of a set of bytes.
 * @s:first param
 * @accept: second param
 *
