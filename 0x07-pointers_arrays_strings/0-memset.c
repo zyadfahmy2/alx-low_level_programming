f832d2926cca38c7a91cb3e3cc860c9658a01bf0
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-03-06 02:49:33 -0800

    README.md

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _memset - function fills the first n bytes of the memory area pointed to by s with the constant byte b
+* _memset - function fills the first n bytes of the memory area
 * @s: pointer to character to be passed (we could pass an array of integer here
 * @b: the value to where the bytes of s will be set
 * @n: the number of bytes to be set
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this project is about arrays, pointers, multidimentiona arrays and strings
