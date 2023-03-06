36da97cd6703013ca5c1e4e3aa054caa783c65f9
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-03-06 05:28:28 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,18 +11,16 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, count;
+	int i, j;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		count = 0;
 		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (haystack[i] != needle[j])
 				break;
-			count++;
 		}
-		if (count == j)
+		if (needle[j] == '\0')
 			return (haystack + i);
 	}
 	return (NULL);
