d180c89406908f8acc3134c330575bfa0dcf67eb
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-03-10 14:05:52 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* @argc: number of arguments + name of the program
+* @argv: array of strings contains arguments + name
+*
+* Return: 0 Always
+*/
+int main(int argc, char *argv[]) __attribute__((unused)),
+{
+	printf("%d\n", argc);
+	return (0);
+}
