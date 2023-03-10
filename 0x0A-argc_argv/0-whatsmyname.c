7d2caead46ff05670785e7508a748060de116be3
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-03-10 13:59:33 -0800

    init

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* @argc: number of arguments + name of the program
+* @argv: array of strings contains arguments + name
+*
+* Return: 0 Always
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+this is a project about passing arguments to the main function
