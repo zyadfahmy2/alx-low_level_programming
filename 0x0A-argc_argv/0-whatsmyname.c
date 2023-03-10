bbef4c412e5453874989aa137f2f786e873bd7ba
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-03-10 14:04:22 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,8 @@
 *
 * Return: 0 Always
 */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s", argv[0]);
+	printf("%s\n", argv[0]);
+	return (0);
 }
