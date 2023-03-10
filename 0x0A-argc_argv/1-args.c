d6abe7f4361dc20054b9837bd376df4d9154ce4a
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-03-10 14:07:14 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
 *
 * Return: 0 Always
 */
-int main(int argc, char *argv[]) __attribute__((unused)),
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc);
 	return (0);
