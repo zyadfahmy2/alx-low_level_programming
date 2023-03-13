671939e2142994baec80ffd7d1fa80dc609c817d
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-03-13 07:28:45 -0700

    init

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,13 +9,15 @@
  */
 char *create_array(unsigned int size, char c)
 {
-  unsigned int i;
-  char *arr;
+    unsigned int i;
+    char *arr;
 
-  arr = malloc(sizeof(char) * size);
-  for (i = 0; i < size; i++)
-  {
-    arr[i] = c;
-  }
-  return (arr);
+    arr = malloc(sizeof(char) * size);
+    if (arr == NULL)
+        return (NULL);
+    for (i = 0; i < size; i++)
+    {
+        arr[i] = c;
+    }
+    return (arr);
 }
