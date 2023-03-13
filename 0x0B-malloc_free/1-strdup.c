f0c672424dd7eacf1b3515f7b4a39a99e2975bc6
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-03-13 07:58:59 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -20,7 +20,7 @@ char *_strdup(char *str)
     if (length == 0)
         return (NULL);
     c = malloc(sizeof(char) * length +1);
-    while (str[i] != '\0)
+    while (str[i] != '\0')
     {
         c[i] = str[i];
         i++;
