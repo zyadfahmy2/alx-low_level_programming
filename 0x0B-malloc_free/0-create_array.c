0681d4046021da6f5181b8b6a010ad69c90ab3f8
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-03-13 07:37:59 -0700

    init

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,8 @@ char *create_array(unsigned int size, char c)
     unsigned int i;
     char *arr;
 
+    if (size == 0)
+        return (NULL);
     arr = malloc(sizeof(char) * size);
     if (arr == NULL)
         return (0);
