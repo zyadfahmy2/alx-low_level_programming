42b4b20454486e027e6c84b02ed9cae83086fd88
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-03-16 05:07:57 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - same as calloc in stdlib
+ * @nmemb: number of the size specified
+ * @size: the actual size in terms of bytes
+ *
+ * Return: pointer to allocated memory
+ * or NULL in case size = 0 or mallo fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+		
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	nmemb = 0;
+	for (; nmemb < size; nmemb++)
+		ptr[nmemb] = 0;
+	return (ptr);
+}
