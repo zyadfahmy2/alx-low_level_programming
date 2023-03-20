ba007b10840a7ecfb5cd86319624b84c50d8d1d1
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-03-20 08:03:52 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,14 +5,21 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ *
+ * Return: NULL in case of failure
+ * and a new dog in case of success
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	
+
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
+	if (name == NULL)
+		name = "(nil)";
+	if (owner == NULL)
+		owner = "(nil)";
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
