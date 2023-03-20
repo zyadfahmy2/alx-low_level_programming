243d925d1437e1593283a8db3c013d1aa96fe71a
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-03-20 03:59:03 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 /**
  * main - Entry point
- * program that prints the name of the file it was compiled from, followed by a new line.
+ * program that prints the name of the file it was compiled from,
+ * followed by a new line.
+ *
+ * RETURN: always EXIT_SUCCESS
  */
-int main (void)
+int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (EXIT_SUCCESS);
 }
