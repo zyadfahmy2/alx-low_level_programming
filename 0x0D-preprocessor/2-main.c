af144abf7f73551daeddab6076d2b76e86aa8a80
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-03-20 03:53:56 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * program that prints the name of the file it was compiled from, followed by a new line.
+ */
+int main (void)
+{
+	printf("%s\n",__FILE__);
+}
