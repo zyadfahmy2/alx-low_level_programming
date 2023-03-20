85649cccae7725c08ba77385e2085762b2ff9c97
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-03-20 08:37:55 -0700

    4-new_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+ * free_dog function that frees dogs
+ * @d: the dog to be freed
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
+	free(d);
+}
