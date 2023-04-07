be5fb4566c678f82e1844e4a2476fc239cbe91b1
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-04-07 05:56:37 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,20 +7,25 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res, i, base;
+	unsigned int res, base, length;
 
-	i = 0;
 	res = 0;
 	base = 1;
+	length = 0;
 	if(b == NULL)
 		return (0);
-	while (!b[i])
+	while(!b[i])
 	{
-		if(b[i] < '0' || b[i] > '9')
+		length++;
+	}
+	length--;
+	while (length > -1)
+	{
+		if(b[length] < '0' || b[length] > '9')
 			return (0);
-		res += base * (b[i] - '0');
+		res += base * (b[length] - '0');
 		base *= 2;
-		i++;
+		length--;
 	}
 	return (res);
 }
