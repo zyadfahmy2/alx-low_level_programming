616eb64a78adfd3565cac41f697603561388ade7
zyadfahmy2 <zyadfahmy2@gmail.com>
2023-04-10 16:32:15 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,16 +9,16 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	size_t r, i, w;
+	ssize_t r, w, fd;
 	char *c;
 
 	if (filename == NULL)
 		return (0);
-	int fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	c = malloc(sizeof(char) * letters);
-	r = read(fd, c, letters)
+	r = read(fd, c, letters);
 	if (r <= 0)
 		return (0);
 	w = write(1, c, letters);
